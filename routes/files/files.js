9fd245fb151f80fb3688b0171af8a6740cb42a70
Christian <ingchristian27@gmail.com>
2025-01-17 16:59:05 -0500

    Update API route and test script pattern to run all tests

--- routes/files/files.js
+++ routes/files/files.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { proxyHTTPHandler } from '../../helpers/http/index.js';
-import { getFiles } from '../../apis/secret/index.js';
+import { getFiles } from '../../apis/files/index.js';
 
 const router = express.Router();
 
-router.get('/files', (req, res) => proxyHTTPHandler(req, res, getFiles));
+router.get('/data', (req, res) => proxyHTTPHandler(req, res, getFiles));
 
 export default router;
